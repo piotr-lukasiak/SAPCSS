0f6e42410c4c9b60df7b3664ac7daa0b48451a98
Piotr Lukasiak <piotr.lukasiak@primeline.ie>
2024-09-16 11:46:34 +0100

    customRFUIbackaction

---
+++ customRFUIbackaction.js
@@ -0,0 +1,7 @@
+window.addEventListener('popstate', function(event) {
+    // Prevent the back navigation
+    event.preventDefault();
+
+    // Send F7 key instead of going back
+    setFKey(7);
+})
